546c16c1162befdf774c28c0ce2a6f262c838dbb
Rodrigo <pittezr.dev@gmail.com>
2023-07-03 11:40:00 -0300

    chore: Types definition

---
+++ app/src/pages/Users/types/User.ts
@@ -0,0 +1,14 @@
+export type User = {
+  id: string;
+  fullName: string;
+  document: string;
+  birthDate: Date;
+  email: string;
+  emailVerified: boolean;
+  mobile: string;
+  zipCode: string;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+};
