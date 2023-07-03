e14a1557dc13b3412dcbb393d8f72df0ee26428b
Rodrigo <pittezr.dev@gmail.com>
2023-07-03 11:34:13 -0300

    feat: React Routes installation and configuration

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+import AppRoutes from "./routes";
+
 function App() {
-  return <>App</>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
