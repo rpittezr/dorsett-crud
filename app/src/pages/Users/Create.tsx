d5afcd883890fa97a6b94c71ca5d42142ae067d9
Rodrigo <pittezr.dev@gmail.com>
2023-07-03 15:55:55 -0300

    feat: Create user function

--- app/src/pages/Users/Create.tsx
+++ app/src/pages/Users/Create.tsx
@@ -1,3 +1,22 @@
+import { Paper, Stack } from "@mui/material";
+
+import Breadcrumbs from "../../components/Breadcrumbs";
+import PageTitle from "../../components/PageTitle";
+
+import Form from "./components/Form";
+
 export default function Create() {
-  return <div>Create</div>;
+  return (
+    <>
+      <Stack sx={{ marginBottom: 2 }}>
+        <PageTitle title="Criar Novo Usuário" />
+        <Breadcrumbs
+          path={[{ label: "Usuários", to: "/users/" }, { label: "Novo" }]}
+        />
+      </Stack>
+      <Paper>
+        <Form />
+      </Paper>
+    </>
+  );
 }
